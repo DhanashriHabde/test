6ce7a1225591b136ec41ea92a19d94a0fee8f908
DhanashriHabde <dhanu224454@gmail.com>
2021-05-20 17:34:10 +0530

    second commit

--- covidhelp/settings.py
+++ covidhelp/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-kb5*=_=us91eeb+jj8c+o3-836#ku37d2!%0g^wzsj(k*2eqjo'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['covidhelpsolapur.herokuapp.com',"localhost"]
 
 
 # Application definition
@@ -48,6 +48,7 @@ X_FRAME_OPTIONS='SAMEORIGIN'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
